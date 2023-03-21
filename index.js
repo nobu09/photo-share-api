7c7a4075f09a6167e148accf43909e96cbe5210e
nobu09 <nobu0922@gmail.com>
2023-03-21 12:42:54 +0900

    index.jsのコメント追加

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
+// typeDefs 変数に文字列としてスキーマを定義
 const typeDefs = `
   type Query {
     totalPhotos: Int!
   }
 `
 
+// totalPhotos というクエリを作成したので、スキーマと同じ名前のリゾルバ関数を定義する必要がある
+// 静的に42を返す
 const resolvers = {
   Query: {
     totalPhotos: () => 42

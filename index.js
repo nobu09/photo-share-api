a92660a7ce62660a9a0674610b0f858ff3d7da67
nobu09 <nobu0922@gmail.com>
2023-03-21 12:48:20 +0900

    ApolloServerを起動する処理を追加

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// apollo-server モジュールを読み込む
+const { ApolloServer } = require(`apollo-server`)
+
 // typeDefs 変数に文字列としてスキーマを定義
 const typeDefs = `
   type Query {
@@ -12,4 +15,16 @@ const resolvers = {
     totalPhotos: () => 42
   }
 }
+
+// サーバーのインスタンスを作成
+// その際、typeDefs（スキーマ）とリゾルバを引数に取る
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+// Webサーバーを起動
+server
+  .listen()
+  .then(({url}) => console.log(`GraphQL Server running on ${url}`))
 

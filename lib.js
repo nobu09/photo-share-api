57c781ee6e0c314e31e6839dc4931d18918d7963
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-06-11 19:01:14 +0900

    npm startするといくつかエラーが発生するので修正

--- lib.js
+++ lib.js
@@ -4,7 +4,7 @@ const requestGithubToken = credentials =>
     {
         method: `POST`,
         headers: {
-            `Content-Type`: `application/json`,
+            'Content-Type': 'application/json',
             Accept: `application/json`
         },
         body: JSON.stringify(credentials)
@@ -14,17 +14,17 @@ const requestGithubToken = credentials =>
       throw new Error(JSON.stringify(error))
   })
 
-  const requestGithubUserAccount = token =>
-    fetch(
-        `https://api.github.com/user?access_token=${token}`
-    ).then(toJSON)
-    .catch(error => {
-        throw new Error(JSON.stringify(error))
-    })
+const requestGithubUserAccount = token =>
+  fetch(
+      `https://api.github.com/user?access_token=${token}`
+  ).then(toJSON)
+  .catch(error => {
+      throw new Error(JSON.stringify(error))
+})
 
-async authorizeWithGithub(credentials) {
-    const { access_token } = await requestGithubToken(credentials)
-    const githubUser = await requestGithubUserAccount(access_token)
+const authorizeWithGithub = async credentials => {
+  const { access_token } = await requestGithubToken(credentials)
+  const githubUser = await requestGithubUserAccount(access_token)
 
-    return { ...githubUser, access_token }
+  return { ...githubUser, access_token }
 }

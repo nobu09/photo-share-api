c6a9f7edb2f40414a78d5b8385a36347aeb32eda
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-06-12 09:17:48 +0900

    Mutationの中を整理

--- resolvers/index.js
+++ resolvers/index.js
@@ -27,48 +27,50 @@ const resolvers = {
     // postPhoto ミューテーションと対応するリゾルバ
     // parent: 親オブジェクト（Mutation）への参照、root or obj、常にリゾルバの第一引数になる
     // args: この操作のために送られたGraphQL引数、{name,description} というオブジェクト
-    postPhoto(parent, args) {
-      // 新しい写真を作成し、idを生成する
-      var newPhoto = {
-        id: _id++,
-        ...args.input,
-        created: new Date()
-      }
-      photos.push(newPhoto)
+    Mutation: {
+      postPhoto(parent, args) {
+        // 新しい写真を作成し、idを生成する
+        var newPhoto = {
+          id: _id++,
+          ...args.input,
+          created: new Date()
+        }
+        photos.push(newPhoto)
   
-      // 新しい写真を返す
-      return newPhoto
-    }
+        // 新しい写真を返す
+        return newPhoto
+      },
 
-    async githubAuth(parent, { code }, { db }) {
-      let {
-        message,
-        access_token,
-        avatar_url,
-        login,
-        name
-      } = await authorizeWithGithub({
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        code
-      })
+      async githubAuth(parent, { code }, { db }) {
+        let {
+          message,
+          access_token,
+          avatar_url,
+          login,
+          name
+        } = await authorizeWithGithub({
+          client_id: process.env.CLIENT_ID,
+          client_secret: process.env.CLIENT_SECRET,
+          code
+        })
     
-      if (message) {
-        throw new Error(message)
-      }
+        if (message) {
+          throw new Error(message)
+        }
     
-      let latestUserInfo = {
-        name,
-        githubLogin: login,
-        githubToken: access_token,
-        avatar: avatar_url
-      }
+        let latestUserInfo = {
+          name,
+          githubLogin: login,
+          githubToken: access_token,
+          avatar: avatar_url
+        }
     
-      const { ops:[user] } = await db
-        .collection('users')
-        .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
+        const { ops:[user] } = await db
+          .collection('users')
+          .replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
     
-      return { user, token: access_token }
+        return { user, token: access_token }
+      },
     },
   
     Photo: {

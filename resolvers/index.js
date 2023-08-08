68c44cece3791f66ae5c4c03962ced49c35c75f5
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-08 23:00:21 +0900

    Subscriptionのスペルミス

--- resolvers/index.js
+++ resolvers/index.js
@@ -29,7 +29,7 @@ const resolvers = {
     },
     Mutation,
 
-    Subscripton: {
+    Subscription: {
       newPhoto: {
         subscribe: (parent, args, { pubsub }) =>
           pubsub.asyncIterator(`photo-added`)

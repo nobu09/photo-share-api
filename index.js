4096b2054fbbaf365031329c35b474e383261c12
nobu09 <11857481+nobu09@users.noreply.github.com>
2023-08-11 16:13:00 +0900

    不要なコードを削除

--- index.js
+++ index.js
@@ -10,48 +10,6 @@ require (`dotenv`).config()
 const typeDefs = readFileSync(`./typeDefs.graphql`, `UTF-8`)
 const resolvers = require(`./resolvers`)
 
-// ユニークIDをインクリメントするための変数
-var _id = 0
-
-// ユーザーのサンプル
-var users = [
-  { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
-  { "githubLogin": "gPlake", "name": "Glen Plake" },
-  { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
-]
-var photos = [
-  {
-    "id": "1",
-    "name": "Dropping the Heart Chute",
-    "description": "The heart chute is one of my favorite chutes",
-    "category": "ACTION",
-    "githubUser": "gPlake",
-    "created": "3-28-1977"
-  },
-  {
-    "id": "2",
-    "name": "Enjoing the sunshine",
-    "description": "The heart chute is one of my favorite chutes",
-    "category": "SELFIE",
-    "githubUser": "gPlake",
-    "created": "1-2-1985"
-  },
-  {
-    "id": "3",
-    "name": "Gunbarrel 25",
-    "description": "25 laps on gunvarrel today",
-    "category": "LANDSCAPE",
-    "githubUser": "sSchmidt",
-    "created": "2018-04-15T19:09:57.308Z"
-  },
-]
-var tags = [
-  { "photoID": "1", "userID": "gPlake" },
-  { "photoID": "2", "userID": "sSchmidt" },
-  { "photoID": "2", "userID": "mHattrup" },
-  { "photoID": "2", "userID": "gPlake" },
-]
-
 async function start() {
   const app = express()
   const MONGO_DB = process.env.DB_HOST
